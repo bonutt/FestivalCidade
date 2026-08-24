0bcddd070c40132d13a0847c61fde4ed3787bba2
luigi bonuccelli <bonuccellibr@gmail.com>
2026-08-24 20:27:20 -0300

    feat: calcula custo bruto do ingresso

--- FestivalCidade/Program.cs
+++ FestivalCidade/Program.cs
@@ -10,4 +10,8 @@ string cupom = Console.ReadLine();
 Console.Write("Pontuacao do clube de fidelidade: ");
 int pontos = int.Parse(Console.ReadLine());
 
+decimal custoBruto = 15.00m + (120.00m * dias);
+
+Console.WriteLine("Valor bruto: " + custoBruto);
+
 Console.ReadLine();
