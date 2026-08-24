d98bec264e538b39e05f125b9d1640c2d8141a73
luigi bonuccelli <bonuccellibr@gmail.com>
2026-08-24 20:28:16 -0300

    feat: aplica cupom de desconto de 10%

--- FestivalCidade/Program.cs
+++ FestivalCidade/Program.cs
@@ -11,7 +11,16 @@ Console.Write("Pontuacao do clube de fidelidade: ");
 int pontos = int.Parse(Console.ReadLine());
 
 decimal custoBruto = 15.00m + (120.00m * dias);
+decimal desconto = 0.00m;
 
+if (cupom.ToUpper() == "ROCK10")
+{
+    desconto = custoBruto * 0.10m;
+}
+
+decimal valorFinal = custoBruto - desconto;
+
+Console.WriteLine("Valor final: " + valorFinal);
 Console.WriteLine("Valor bruto: " + custoBruto);
 
 Console.ReadLine();
